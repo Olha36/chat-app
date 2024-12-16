412595535a715a4729aa20fe3fadd9eceb79c255
Olha36 <olhakucheruk36@gmail.com>
2024-12-16 17:38:23 +0100

    establish mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const cors = require("cors"); // allows to communicate with frontend
+const cors = require('cors'); // allows to communicate with frontend
+const mongoose = require('mongoose');
 
 const app = express();
+require('dotenv').config();
 
 app.use(express.json()); // allows to receive and send json data
 app.use(cors());
 
 const port = process.env.PORT || 3000;
+const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
-    console.log(`Server is running on  port ${port}`);
-    
-})
+  console.log(`Server is running on  port ${port}`);
+});
+
+mongoose
+  .connect(uri)
+  .then(() => console.log('MongoDb connection established'))
+  .catch((error) => console.log('MongoDB connection failed:', error));
