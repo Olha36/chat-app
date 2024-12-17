167ca41028d9cee8468dd3d838df8413c5b9a1aa
Olha36 <olhakucheruk36@gmail.com>
2024-12-17 10:10:37 +0100

    set up a get root, define the schema for a MongoDB collection, create routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors'); // allows to communicate with frontend
 const mongoose = require('mongoose');
+const userRoute = require('./Routes/userRoute');
 
 const app = express();
 require('dotenv').config();
 
 app.use(express.json()); // allows to receive and send json data
 app.use(cors());
+app.use('/api/users', userRoute)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our chat app API');
+});
+
+
 
 const port = process.env.PORT || 3000;
 const uri = process.env.ATLAS_URI;
