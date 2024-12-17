f79d5ef7339c807e59aa14bdd71a9722771f16b8
Olha36 <olhakucheruk36@gmail.com>
2024-12-17 16:13:35 +0100

    get user and check if email, password and name

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,7 +1,17 @@
 const userModel = require('../Models/userModel');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-const registerUser = (req, res) => {
-  res.send('Register');
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(400).json('User with the given email already exists..');
+  if (!name || !email || !password) return res.status(400).json('All fields are required..');
+  if (!validator.isEmail(email)) return res.status(400).json('Invalid email..');
+  if (!validator.isStrongPassword(password)) return res.status(400).json('The password must be stronger..');
+  
 };
 
 module.exports = { registerUser };
