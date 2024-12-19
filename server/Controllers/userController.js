01c985f48b5342e92dc9c9b2afba6f4dda9c6e3a
Olha36 <olhakucheruk36@gmail.com>
2024-12-19 19:38:48 +0100

    create a loginUser controller for checking  password and login

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -32,4 +32,22 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await userModel.findOne({ email });
+
+    if (!user) return res.status(400).json('Invalid email or password..');
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) return res.status(400).json('Invalid email or password..');
+
+    const token = createToken(user._id);
+    res.status(200).json({ _id: user, name: user.name, email, token });
+  } catch (error) {
+    console.log('Error is:', error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
